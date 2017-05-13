07305f19c862805bc035fbe64c2069c372459cef
Kat Fisher <kfisher2@trinity.edu>
2017-05-13 10:50:07 -0700

    unrelated changes

--- hello_world.cu
+++ hello_world.cu
@@ -32,4 +32,4 @@ __global__  indicates function that 1) runs on DEVICE , 2) called from the HOST
 
 <<<   >>>   indicates function that    runs on DEVICE and is called from the HOST
 
-*/
+*////////
